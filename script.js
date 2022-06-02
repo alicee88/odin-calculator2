47f1d4400c0f8e28eb374b6f8d0c9e38cf6b1164
alicee88 <alice.easey@gmail.com>
2022-06-02 14:06:43 +0100

    Improved operations, add styling

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const displayDiv = document.querySelector('.display');
 const opDisplayDiv = document.querySelector('.op-display');
 const buttons = document.querySelectorAll('.input');
+const decimal = document.querySelector('.decimal');
 
 let displayValue = '';
-let computedValue = 0;
 let operator = '';
 let leftSide = 0;
-let rightSide = 0;
+let prevKey = '';
 
 const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
@@ -14,58 +14,94 @@ const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
 function operate(op, x, y) {
-    console.log("OPERATE: " + op + " X: " + x + " Y: " + y);
+    let answer = 0;
     switch (op) {
         case '+':
-            return add(x, y);
+            answer = add(x, y);
+            break;
         case '-':
-            return subtract(x, y);
+            answer = subtract(x, y);
+            break;
         case '*':
-            return multiply(x, y);
+            answer = multiply(x, y);
+            break;
         case '/':
-            return divide(x, y);
+            if (y === 0) {
+                return 'No dividing by 0';
+            }
+            answer = divide(x, y);
+            break;
         default:
             return 'illegal operator';
     }
+    return answer.toPrecision(9) / 1;
 }
 
 function updateDisplay(e) {
-    if (operator) {
-        console.log("Got operator " + operator);
-        if (e.target.classList.contains('num')) {
-            console.log("Clicked number " + e.target.dataset.key);
+    if (e.target.classList.contains('num')) {
+        if (displayValue.length < 10) {
             displayValue += e.target.dataset.key;
-            displayDiv.textContent = displayValue;
+            displayDiv.textContent = (Number)(displayValue);
         }
-        else if (e.target.classList.contains('op') || e.target.dataset.key === '=') {
-            console.log("Clicked operator " + e.target.dataset.key);
-            computedValue = operate(operator.dataset.key, parseInt(leftSide), parseInt(displayValue));
-            displayDiv.textContent = computedValue;
-            leftSide = computedValue;
-            displayValue = '';
+        prevKey = 'num';
+    } else if (e.target.classList.contains('op')) {
+        if (operator) {
+            if (displayValue) {
+                if (leftSide) {
+                    displayValue = operate(operator.dataset.key, parseInt(leftSide), parseInt(displayValue));
+                    displayDiv.textContent = displayValue;
+                }
+                leftSide = displayValue;
+                displayValue = '';
+                decimal.disabled = false;
+            }
             operator.classList.remove('selected');
-            operator = e.target;
-            e.target.classList.add('selected');
         }
-    }
-    else {
-        console.log("No operator");
-        if (e.target.classList.contains('num')) {
-            console.log("Clicked a number " + e.target.dataset.key);
-            displayValue += e.target.dataset.key;
+        else {
+            if (prevKey === 'num') {
+                leftSide = displayValue;
+                displayValue = '';
+                decimal.disabled = false;
+            }
+        }
+        operator = e.target;
+        operator.classList.add('selected');
+        prevKey = 'op';
+    } else if (e.target.dataset.key === '=') {
+        if (operator) {
+            displayValue = operate(operator.dataset.key, (Number)(leftSide), (Number)(displayValue));
             displayDiv.textContent = displayValue;
-        }
-        else if (e.target.classList.contains('op')) {
-            e.target.classList.add('selected');
-            //operator = e.target.dataset.key;
-            operator = e.target;
-            console.log("SET OPERATOR TO " + operator);
             leftSide = displayValue;
             displayValue = '';
+            operator.classList.remove('selected');
+            operator = '';
+            decimal.disabled = false;
+        }
+        prevKey = '=';
+    } else if (e.target.dataset.key === 'clr') {
+        if (operator) {
+            operator.classList.remove('selected');
+            operator = '';
+        }
+        displayValue = '';
+        leftSide = 0;
+        prevKey = '';
+        displayDiv.textContent = '';
+        decimal.disabled = false;
+    } else if (e.target.dataset.key === '.') {
+        if (!displayValue) {
+            displayValue = '0';
+        }
+        displayValue += '.';
+        displayDiv.textContent = displayValue;
+        e.target.disabled = true;
+    } else if (e.target.dataset.key === 'del') {
+        if (displayValue && prevKey === 'num') {
+            displayValue = displayValue.slice(0, - 1);
+            displayDiv.textContent = displayValue;
         }
 
     }
-
 }
 
 buttons.forEach(button => addEventListener('click', updateDisplay));
